89f3424421c73a428b6a16144cf65c29238792b9
saadaoui imane <saadaouiimane2004@gmail.com>
2024-12-20 10:22:00 +0100

    Initial commit

--- src/main/java/FizzBuzz/fizzbuzz.java
+++ src/main/java/FizzBuzz/fizzbuzz.java
@@ -3,7 +3,10 @@ package FizzBuzz;
 public class fizzbuzz {
 
 
-       public static String de(int nbr) {
+    public static String de(int nbr) {
+        if (nbr == 3) {
+            return "Fizz";
+        }
         return "1";
     }
 
--- src/test/java/FizzBuzz/fizzbuzzTest.java
+++ src/test/java/FizzBuzz/fizzbuzzTest.java
@@ -9,5 +9,11 @@ public class fizzbuzzTest {
     public void fizzBuzz_de_1_devrait_retourner_1() {
         assertThat(fizzbuzz.de(1)).isEqualTo("1");
     }
+
+    @Test
+    void fizzBuzz_de_3_devrait_retourner_Fizz() {
+        assertThat(fizzbuzz.de(3)).isEqualTo("Fizz");
+    }
+
 }
 
